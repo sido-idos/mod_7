bad19255c2ce67e18ddab8764d60b7822509aecf
sido-idos <alejandro.abarca02@inacapmail.cl>
2026-03-22 15:01:21 -0300

    Modificaciones de html para usuarios y operaciones, forms, css y agregar imagenes

--- apps/wallet/forms.py
+++ apps/wallet/forms.py
@@ -5,8 +5,19 @@ class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control bg-dark text-light'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control bg-dark text-light'}),
+            'saldo': forms.NumberInput(attrs={'class': 'form-control bg-dark text-light'}),
+        }
 
 class OperacionForm(forms.ModelForm):
     class Meta:
         model = Operacion
         fields = '__all__'
+        widgets = {
+            'usuario': forms.Select(attrs={'class': 'form-select bg-dark text-light'}),
+            'tipo': forms.Select(attrs={'class': 'form-select bg-dark text-light'}),
+            'monto': forms.NumberInput(attrs={'class': 'form-control bg-dark text-light'}),
+            'fecha': forms.DateInput(attrs={'class': 'form-control bg-dark text-light', 'type':'date'}),
+        }
